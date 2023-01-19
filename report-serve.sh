dd1b53d07d4eaad3392c574e0d762633dab1217f
Xavier RODRIGUEZ <xavier.rodriguezalvarez@gmail.com>
2023-01-19 16:32:54 +0100

    Create report-serve.sh

---
+++ report-serve.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Start web server
+docker run --rm -d --name doc-server \
+    -v $PWD:/usr/share/nginx/html:ro \
+    -v $PWD/nginx.conf:/etc/nginx/conf.d/default.conf:ro \
+    -p 8080:80 nginx
+
