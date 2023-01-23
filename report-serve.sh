9cb68ccbeede9eb7cfbbba771c138079ed1bd56c
Xavier RODRIGUEZ <xavier.rodriguezalvarez@gmail.com>
2023-01-23 07:53:59 +0100

    Update report-serve.sh

--- report-serve.sh
+++ report-serve.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start web server
-docker run --rm -d --name doc-server \
+docker run --rm -d --name robot-report-server \
     -v $PWD:/usr/share/nginx/html:ro \
     -v $PWD/nginx.conf:/etc/nginx/conf.d/default.conf:ro \
     -p 8080:80 nginx
